df0ae7dfc1f81979b63e251849ff419199c75453
Jae Young Park <jaeyoung.p@ingstory.kr>
2020-02-09 19:53:32 +0900

    update script

--- script/profile.sh
+++ script/profile.sh
@@ -19,25 +19,6 @@ function find_idle_profile(){
     fi
 
     echo "${IDLE_PROFILE}"
-
-#    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
-#
-#    if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
-#    then
-#        CURRENT_PROFILE=real2
-#    else
-#        CURRENT_PROFILE=$(curl -s http://localhost/profile)
-#    fi
-#
-#    if [ ${CURRENT_PROFILE} == real1 ]
-#    then
-#      IDLE_PROFILE=real2
-#    else
-#      IDLE_PROFILE=real1
-#    fi
-#
-#    echo "${IDLE_PROFILE}"
-
 }
 
 # 쉬고 있는 profile의 port 찾기
