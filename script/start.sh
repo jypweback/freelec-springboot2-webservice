3c6df2a2bced3a545991d6040c71c105da339caf
Jae Young Park <jaeyoung.p@ingstory.kr>
2020-02-09 19:01:09 +0900

    script update

--- script/start.sh
+++ script/start.sh
@@ -14,7 +14,7 @@ echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> $JAR_NAME 에 실행권한 추가"
-chmod +x $REPOSITORY/$JAR_NAME
+chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 IDLE_PROFILE=$(find_idle_profile)
@@ -26,4 +26,4 @@ nohup java -jar \
         /home/ec2-user/app/application-oauth.yml,\
         /home/ec2-user/app/application-real-db.yml \
         -Dspring.profiles.active=$IDLE_PROFILE \
-        $REPOSITORY/$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
