0b40349644ebaee24197ab89342ba8fa670b04a7
Jae Young Park <jaeyoung.p@ingstory.kr>
2020-03-16 13:46:33 +0900

    title change

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,18 +1,18 @@
 var index = {
-    init : function () {
-        var _this = this;
-        $('#btn-save').on('click', function () {
-            _this.save();
-        });
-        $('#btn-update').on('click', function () {
-            _this.update();
-        });
-        $('#btn-delete').on('click', function () {
-            _this.delete();
-        });
-    },
+        init : function () {
+            var _this = this;
+            $('#btn-save').on('click', function () {
+                _this.save();
+            });
+            $('#btn-update').on('click', function () {
+                _this.update();
+            });
+            $('#btn-delete').on('click', function () {
+                _this.delete();
+            });
+        },
 
-    save : function () {
+        save : function () {
         var data = {
             title : $('#title').val(),
             author : $('#author').val(),
